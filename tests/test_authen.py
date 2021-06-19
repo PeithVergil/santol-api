5786af564ede255ec21d07ef0af1b39adac307d9
Peith Vergil <peith.vergil@gmail.com>
2021-06-19 23:06:19 +0800

    Perform clean-up in fixtures.

--- tests/test_authen.py
+++ tests/test_authen.py
@@ -1,30 +1,40 @@
+import pytest
+
 from .client import client
 from .settings import USERS
 
 from santol.authen import delete_user, delete_token
 
 
-def test_users_create():
-    """
-    Running the test on its own:
-    
-        pytest tests/test_authen.py::test_users_create
-    """
+@pytest.fixture
+def create_user_response():
     user = USERS[0]
 
     response = client.post('/auth/users', json={
         'username': user['username'],
         'password': user['password'],
     })
-    assert response.status_code == 200
+
+    yield response
 
     data = response.json()
+
+    assert delete_user(data['id']) is True
+
+
+def test_users_create(create_user_response):
+    """
+    Running the test on its own:
+    
+        pytest tests/test_authen.py::test_users_create
+    """
+    assert create_user_response.status_code == 200
+
+    data = create_user_response.json()
 
     assert data['id']         is not None
     assert data['username']   is not None
     assert data['created_at'] is not None
-
-    assert delete_user(data['id']) is True
 
 
 def test_users_create_duplicate(users):
@@ -46,24 +56,32 @@ def test_users_create_duplicate(users):
     assert data['detail'] == 'Duplicate entry'
 
 
-def test_users_authenticate(users):
-    """
-    Running the test on its own:
-
-        pytest tests/test_authen.py::test_users_authenticate
-    """
+@pytest.fixture
+def authenticate_user(users):
     user = USERS[0]
 
     response = client.post('/auth/authenticate', json={
         'username': user['username'],
         'password': user['password'],
     })
-    assert response.status_code == 200
+
+    yield response
 
     data = response.json()
+
+    assert delete_token(data['value']) is True
+
+
+def test_users_authenticate(authenticate_user):
+    """
+    Running the test on its own:
+
+        pytest tests/test_authen.py::test_users_authenticate
+    """
+    assert authenticate_user.status_code == 200
+
+    data = authenticate_user.json()
 
     assert data['value']      is not None
     assert data['created_at'] is not None
     assert data['expired_at'] is not None
-
-    assert delete_token(data['value']) is True
